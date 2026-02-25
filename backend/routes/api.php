b90238d4b27a625da8fcc01dcd689ca88692ea0c
MwauStephen <stephenmwau41@gmail.com>
2026-02-25 16:06:57 +0300

    added comments on routes for identification

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,11 +9,13 @@ use App\Http\Controllers\Api\TransactionController;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//route for creating a user
 Route::post('/users', [UserController::class, 'store']);
+//route for getting a user profile
 Route::get('/users/{user}/profile', [UserController::class, 'show']);
-
+//route for creating a wallet
 Route::post('/users/{user}/wallets', [WalletController::class, 'store']);
+//route for getting a wallet
 Route::get('/wallets/{wallet}', [WalletController::class, 'show']);
-
+//route for creating a transaction
 Route::post('/wallets/{wallet}/transactions', [TransactionController::class, 'store']);
