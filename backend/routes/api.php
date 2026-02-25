846cc0d6466d71883c84f8e2479dd895c27bf6d1
MwauStephen <stephenmwau41@gmail.com>
2026-02-25 15:08:07 +0300

    Configured API routes for tracker

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\WalletController;
+use App\Http\Controllers\Api\TransactionController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{user}/profile', [UserController::class, 'show']);
+
+Route::post('/users/{user}/wallets', [WalletController::class, 'store']);
+Route::get('/wallets/{wallet}', [WalletController::class, 'show']);
+
+Route::post('/wallets/{wallet}/transactions', [TransactionController::class, 'store']);
